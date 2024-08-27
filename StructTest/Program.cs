01fc174cc461d8c9fb317444cb6016473781ec72
AwakeningSoul <weathergoals@gmail.com>
2024-08-27 17:34:00 +0100

    Add project files.

---
+++ StructTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StructTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create and initialize an instance of the Number struct
+            Number myNumber = new Number { Amount = 123.45m };
+
+            // Print the amount to the console
+            Console.WriteLine($"The amount is: {myNumber.Amount}");
+
+            // Keep the console window open until the user presses a key
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StructTest/StructNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructTest
+{
+    // Define the struct called Number
+    public struct Number
+    {
+        // Property to hold the amount of type decimal
+        public decimal Amount { get; set; }
+    }
+}
